d2fa35ee8c750ee59e9b47310d8f19508e9923c9
YanaBaharevich <w68369@student.wsiz.edu.pl>
2023-03-26 21:18:40 +0200

    Try1

--- js.js
+++ js.js
@@ -45,13 +45,14 @@ Zadanie 3.1
 var komp=Math.ceil(Math.random() * 10);
 const ja=prompt("Podaj liczbę:");
 var f=ja-komp;
-if(komp===ja){
+if(komp==ja){
    console.log(komp)
    console.log(ja)
    console.log("Dobra robota")
    document.getElementById("ansR").
    innerHTML="Dobra robota";
-}else{
+}else(komp!=ja)
+{
     console.log(komp)
     console.log(ja)
     console.log("Nie trafiłaś :<")
@@ -74,3 +75,71 @@ console.log("Liczba 1 jest najweksza "+l1)
 }else{
     console.log("Liczba 3 jest najweksza "+l3)
 }
+/*
+Zadanie 5
+*/
+var k1=prompt("Podaj 1 liczbę:");
+var k2=prompt("Podaj 2 liczbę:");
+
+while(k1!=k2){
+    if(k1>k2){
+        k1-=k2
+    }else if(k1<k2){
+        k2-=k1}
+    else{c
+        onsole.log("NWD jest:"+k1)
+    }
+}
+console.log(k1)
+document.getElementById("nwdR").
+innerHTML="NWD jest:"+k1;
+/*
+Zadanie 6
+*/
+var Student1={}
+var Student2={}
+var Student3={}
+Student1.imię="Jakub";
+Student1.nazwisko="Wojcech";
+Student1.wiek="20";
+
+Student2.imię="Kacper";
+Student2.nazwisko="Jamiński";
+Student2.wiek="21";
+
+Student3.imię="Barbara";
+Student3.nazwisko="Mroczek";
+Student3.wiek="19";
+console.log(Student1)
+console.log(Student2)
+console.log(Student3)
+/*
+Zadanie 7
+*/
+var StudentA={}
+var StudentB={}
+var StudentC={}
+var nameA=prompt("Wpisz imię dla studenta A:");
+StudentA.imię=nameA;
+var surnameA=prompt("Wpisz nazwisko dla studenta A:");
+StudentA.nazwisko=surnameA;
+var ageA=prompt("Wpisz wiek dla studenta A:");
+StudentA.wiek=ageA;
+
+var nameB=prompt("Wpisz imię dla studenta B:");
+StudentB.imię=nameB;
+var surnameB=prompt("Wpisz nazwisko dla studenta B:");
+StudentB.nazwisko=surnameB;
+var ageB=prompt("Wpisz wiek dla studenta B:");
+StudentB.wiek=ageB;
+
+var nameC=prompt("Wpisz imię dla studenta C:");
+StudentC.imię=nameC;
+var surnameC=prompt("Wpisz nazwisko dla studenta C:");
+StudentC.nazwisko=surnameC;
+var ageC=prompt("Wpisz wiek dla studenta C:");
+StudentC.wiek=ageC;
+
+console.log(StudentA)
+console.log(StudentB)
+console.log(StudentC)
