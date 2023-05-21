da9b4ce8f2938af478cbaaf89495857e39ccc267
YanaBaharevich <w68369@student.wsiz.edu.pl>
2023-05-21 17:39:22 +0200

    zad 1,3

--- js6.js
+++ js6.js
@@ -22,3 +22,28 @@ class prostokat{
 
 }
 }
+const pr1=new prostokat(4,10,"numer 1");
+const pr2=new prostokat(5,5,"numer 2");
+const pr3=new prostokat(5,5,"numer 3");
+console.log(pr1.oblicz(),pr1.oblicz2());
+console.log(pr2.oblicz(),pr2.oblicz2());
+console.log(pr3.oblicz(),pr3.oblicz2());
+console.log("Większy prostokąt to "+pr1.porownaj(pr2).nazwa);
+console.log("Większy prostokąt to "+pr2.porownaj(pr3).nazwa);
+
+//Zadanie 3
+class Trapez{
+    constructor(nazwa,wysokość,podstawa1,podstawa2){
+        this.wysokość=wysokość;
+        this.nazwa=nazwa;
+        this.podstawa1=podstawa1;
+        this.podstawa2=podstawa2;
+    }
+    pole(){
+    return 1/2*(this.podstawa1+this.podstawa2)*this.wysokość;
+    }
+}
+
+const tr1=new Trapez(4,10,"numer 1");
+const tr2=new Trapez(5,5,"numer 2");
+const rt3=new Trapez(5,5,"numer 3");
