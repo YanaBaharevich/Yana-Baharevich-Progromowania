da8038d20eee2946c3f2d7521e9c07d2c988bd6c
YanaBaharevich <w68369@student.wsiz.edu.pl>
2023-05-21 19:45:58 +0200

    1

--- js7-8.js
+++ js7-8.js
@@ -1,12 +1,71 @@
-function wartosc(firld){
-    const passwordr=/!@#$%^&*/;
-    if (!passwordr.test(firld.value)){
-        firld.setCustomValidity("...");
-        return true;
-    }
-    else{
-        firld.setCustomValidity('');
-        return false
-    }
+//Zadanie 1
 
-}
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (validForm()) {
+        alert('Sukces');
+    }
+});
+
+const nameField = document.querySelector("[name='name']");
+nameField.addEventListener('input', () => {
+    requiredValidation(nameField);
+    minLengthValidation(nameField, 2);
+});
+
+const surnameField = document.querySelector("[name='surname']");
+surnameField.addEventListener('input', () => {
+    requiredValidation(surnameField);
+    minLengthValidation(surnameField, 2);
+});
+
+
+const emailField = document.querySelector("[name='email']");
+emailField.addEventListener('input', () => {
+    requiredValidation(emailField);
+    minLengthValidation(emailField, 2);
+    emailValidation(emailField);
+});
+
+
+const passwordField = document.querySelector("[name='password']");
+passwordField.addEventListener('input', () => {
+    requiredValidation(passwordField);
+});
+
+
+const sexFields = document.getElementsByName('sex');
+
+sexFields.forEach(field => {
+    field.addEventListener('change', () => {
+        validSexFileds()
+    });
+});
+
+const phoneField = document.querySelector("[name='phone']");
+phoneField.addEventListener('input', () => {
+    requiredValidation(phoneField);
+    minLengthValidation(phoneField, 9);
+    maxLengthValidation(phoneField, 9);
+});
+
+phoneField.addEventListener('keypress', (event) => {
+    var charCode = (event.which) ? event.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        event.preventDefault();
+    }
+    return true;
+});
+
+const birthDateField = document.querySelector("[name='birthDate']");
+birthDateField.addEventListener('input', () => {
+    requiredValidation(birthDateField);
+    validAge();
+});
+
+const confirmPasswordField = document.querySelector("[name='confirm-password']");
+confirmPasswordField.addEventListener('input', () => {
+    validConfirmPassword();
+});
